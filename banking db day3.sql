b54d9acdfe09ebd4f26174397a64ce5384a14e95
rajalkshmi1-dotcom <rajalkshmi1@gmail.com>
2026-08-20 13:08:02 +0530

    update file

---
+++ banking db day3.sql
@@ -0,0 +1,156 @@
+create database BankingDB;
+use BankingDB;
+select database();
+CREATE TABLE Customers
+(
+    CustomerID INT primary key,      
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    accountcreationdate date
+);
+drop table customers;
+describe customers; 
+create table transactions (
+transactionid int,
+transactiondate date,
+amount decimal (10,2),
+transactiontype varchar (20) );
+create table branches (
+branchid int,
+branchname varchar (100),
+branchaddress varchar (200),
+branchphone varchar (15)
+);
+create table accountbranches (
+assignmentdate date
+);
+create table loans (
+loanid int,
+loanamount decimal (10,2),
+intrestrate decimal (5,2),
+startdate date,
+enddate date
+); 
+use sakila; 
+
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+DROP TABLE AccountBranches;
+ALTER TABLE Customers
+ADD PRIMARY KEY (CustomerID);
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+describe customers;
+ALTER TABLE Accounts
+ADD CustomerID INT;
+alter table transactions
+add accountid int;
+alter table transactions
+add accountid int;
+alter table accounts
+add constraint pk_accounts
+primary key(accountid);
+alter table transactions
+add constraint pk_transactions
+primary key(transactionid);
+alter table transactions
+add constraint fk_transactions_accounts
+foreign key(accountid)
+references accounts(accountid);
+ALTER TABLE loans
+ADD CustomerID INT;
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+alter table loans
+add constraint fk_loans_customers
+foreign key (customerid)
+references customers(customerid);
+desc loans;
+alter table loans
+add constraint pk_loans
+primary key(loanid);
+select * from customers;
+desc customers;
+alter table customers add column pan varchar(20);
+alter table customers add column pan varchar(20) first;
+alter table customers add column pan varchar(20) after email;
+alter table customers modify column pan varchar(50);
+
+alter table customers drop column pan;
+ALTER TABLE Accounts
+ADD branchID INT;
+alter table branches
+add primary key (branchid);
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_branch1
+FOREIGN KEY (branchID)
+REFERENCES branches(branchID);
+INSERT INTO Customers
+VALUES
+(101,'Rahul','Sharma','rahul@gmail.com','9876543210','2006-05-15','1998-04-15');
+
+INSERT INTO Customers
+VALUES
+(102,'Bhavyaa','Shree','bhavyaa@gmail.com','7598474217','2026-11-18','2002-07-15');
+
+INSERT INTO Customers
+VALUES
+(102,'Bhavyaa','Shree','bhavyaa@gmail.com','7598474217','2026-11-18','2002-07-15');
+insert into accounts
+(accountid, customerid, accounttype, balance)
+values
+(201,101,'Savings',25000);
+
+insert into accounts
+(accountid, customerid, accounttype, balance)
+values
+(202,102,'current',10000);
+INSERT INTO Customers(customerid,email,phone)
+values(103,'hi@gmail.com',1234567891),(104,'hello@gmail.com',0987654321);
+
+
+
+
+
+UPDATE Customers
+SET Phone='9999999999'
+WHERE CustomerID=101;
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+UPDATE Customers
+SET Email='rahul.sharma@gmail.com'
+WHERE CustomerID=101;
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+DELETE FROM Accounts
+WHERE AccountID = 202;
+SELECT * FROM Accounts;
